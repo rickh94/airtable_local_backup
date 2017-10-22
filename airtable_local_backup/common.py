cd047b7259a1324ae29b97db617b202278275cae
Rick Henry <fredericmhenry@gmail.com>
2017-10-21 22:39:38 -0400

    fix some bugs
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- airtable_local_backup/common.py
+++ airtable_local_backup/common.py
@@ -8,11 +8,11 @@ def _findkeys(node, kv):
     """
     if isinstance(node, list):
         for i in node:
-            for x in findkeys(i, kv):
+            for x in _findkeys(i, kv):
                 yield x
     elif isinstance(node, dict):
         if kv in node:
             yield node[kv]
         for j in node.values():
-            for x in findkeys(j, kv):
+            for x in _findkeys(j, kv):
                 yield x
