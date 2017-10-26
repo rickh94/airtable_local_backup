9fea871b92fd968221857606c106f0d1a2944976
Rick Henry <fredericmhenry@gmail.com>
2017-10-26 01:32:46 -0400

    Add method to create DownloadTable objects for all tables specified in config

--- airtable_local_backup/runner.py
+++ airtable_local_backup/runner.py
@@ -2,6 +2,8 @@ from ruamel.yaml import YAML
 import fs
 from fs_s3fs import S3FS
 
+from .download import DownloadTable
+
 
 class Runner(object):
     def __init__(self, path, *, filesystem=None):
@@ -16,3 +18,12 @@ class Runner(object):
             filesystem = fs.open_fs('/')
         with filesystem.open(path, 'r') as configfile:
             self.config = yaml.load(configfile)
+
+    def _create_backup_tables(self):
+        for table in self.config['Tables']:
+            yield DownloadTable(
+                base_key=self.config['Airtable Base Key'],
+                table_name=table['Name'],
+                api_key=self.config['Airtable API Key'],
+                compression=self.config['Attachments']['Compression']
+            )
