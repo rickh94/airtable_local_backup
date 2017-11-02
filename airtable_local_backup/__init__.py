3ba62b08c37c9324f9b1f3ed7a45c066058f87d0
Rick Henry <fredericmhenry@gmail.com>
2017-11-02 00:14:59 -0400

    move public classes/functions to package namespace
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- airtable_local_backup/__init__.py
+++ airtable_local_backup/__init__.py
@@ -2,3 +2,7 @@ __version__ = '0.1a0'
 __docurl__ = 'not_here_yet'
 
 from .runner import Runner
+from .file_io import write_out_backup
+from .file_io import join_files
+from .file_io import write_to_file
+from .download import DownloadTable
