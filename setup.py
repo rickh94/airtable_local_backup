647a2edb631ad82fc07b3a961a8ede8efd228ea4
Rick Henry <fredericmhenry@gmail.com>
2017-11-10 20:06:45 -0500

    ugh
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         'fs',
         'fs-s3fs',
         'better_exceptions',
-        'ruamel.yaml'
     ],
     tests_require=['pytest', 'pytest-cov'],
     # setup_requires=['pytest-runner'],
